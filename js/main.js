a3ab4cfd7ba76816020a0de7a5eb1b1409ade630
elek3k <48998214+elek3k@users.noreply.github.com>
2020-03-13 23:10:51 +0300

    feat: add function close modal for click on esc and outside it

--- js/main.js
+++ js/main.js
@@ -3,40 +3,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
 
+  // присваивает модальному окну новый класс
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
 
-  modalBtn.forEach(element => {
-    element.addEventListener ('click', switchModal);
+  // присвоение класса .modal--visible для модального окна при нажатии не кнопку
+  modalBtn.forEach(e => {
+    e.addEventListener ('click', switchModal);
   });
 
+  // закрытие модального окна по нажатию на крестик
   closeBtn.addEventListener('click', switchModal);
 
+  // закрытие модального окна по нажатию мышки вне диалогового  окна
+  document.addEventListener("click", e => {
+    if (e.target == modal) {
+        modal.classList.remove("modal--visible");
+    };
+  });
+
+  // закрытие модального окна по нажатию клавиши esc
+  document.addEventListener('keydown', e => {
+    if (e.keyCode === 27 ) {
+      modal.classList.remove("modal--visible");
+    }
+  });
+
 });
-
-document.addEventListener('keypress', (event) => {
-  if (event.keyCode == 27) { 
-    $('.modal').switchModal();
-  }
-});
-// document.addEventListener('keypress', function (e) {
-//     if(e.keyCode === 27) 
-//   }); 
-// $(document).keydown(function(event) { 
-//   if (event.keyCode == 27) { 
-//     $('.modal').hide();
-//   }
-// });
-
-// document.addEventListener('keypress', (event) => {
-//   if (event.keyCode === 27 {
-//     switchModal
-//   });
-// });
-
-// document.click(function (event) {
-//   if ($(event.target).is('.modal')) {
-//     switchModal();
-//   }
-// });

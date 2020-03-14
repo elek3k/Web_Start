54863818783fb12ddd08fff683ddd2fd987efee3
elek3k <48998214+elek3k@users.noreply.github.com>
2020-03-15 01:02:35 +0300

    feat: add section examples with slaider

--- js/main.js
+++ js/main.js
@@ -82,6 +82,26 @@ $(document).ready(function () {
 	scrollUp.click(function(){
 		$('html, body').animate({scrollTop: 0}, 600);
 		return false;
-	});
+  });
+  
+  // инициализация слайдера
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
 
+  var next = $('.swiper-button-next'),
+      prev = $('.swiper-button-prev'),
+      bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 15 + bullets.width() + 15)
+  bullets.css('left', prev.width() + 15)
 });
